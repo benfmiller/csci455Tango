c13ecaab71e8b1a479e2d194809057bb9947d67b
Ben Miller <benfmiller132@gmail.com>
2022-04-07 20:01:58 -0600

    builds tree

--- dialog/main_dialog.py
+++ dialog/main_dialog.py
@@ -3,22 +3,23 @@ import pyttsx3
 import re
 import csv
 import string
+from pprint import pprint
 
 
 class Listener:
     microphone: sr.Microphone
-    listener: sr.Recognizer
+    listener_rec: sr.Recognizer
     using_console = False
 
     def __init__(self, audio=True):
         if audio is True:
             with sr.Microphone() as source:
                 self.speaker = source
-                self.listener = sr.Recognizer()
+                self.listener_rec = sr.Recognizer()
                 print("Adjusting to ambient noise")
-                self.listener.adjust_for_ambient_noise(source, duration=2)
-                self.listener.energy_threshold = 3000
-                self.listener.dynamic_energy_threshold = True
+                self.listener_rec.adjust_for_ambient_noise(source, duration=2)
+                self.listener_rec.energy_threshold = 3000
+                self.listener_rec.dynamic_energy_threshold = True
         else:
             self.using_console = True
 
@@ -29,9 +30,9 @@ class Listener:
                 user_input = input("Human: ").lower()
             try:
                 print("listening")
-                audio = self.listener.listen(self.microphone)
+                audio = self.listener_rec.listen(self.microphone)
                 print("Got audio")
-                words = self.listener.recognize_google(audio)
+                words = self.listener_rec.recognize_google(audio)
                 if isinstance(words, str):
                     words = words.lower()
                 user_input = words
@@ -69,6 +70,10 @@ class Speaker:
 class DocumentNode:
     doc_line: str
     good_line: bool = True
+    level: int
+    inputs: list[str]
+    outputs: list[str]
+    child_nodes: list[any] = []
 
     def __init__(self, document_line: str) -> None:
         self.doc_line = document_line
@@ -80,12 +85,23 @@ class DocumentNode:
             print(f"Error: line must start with 'u': {line}")
         if line[1] not in string.digits:
             line = "u0" + line[1:]
+        try:
+            self.level = int(line[1 : line.index(":")])
+        except Exception as e:
+            print(f"Illegal level in {self.doc_line}")
+
         # TODO:
+        # square brackets in input are multiple input options
+        # Everything after colon is output
+        # Extract variable from underscore, assign to "$..."
+
+    def __repr__(self) -> str:
+        return self.doc_line
 
 
 class Document:
     contents: list[str]
-    root_nodes: list[DocumentNode]
+    root_nodes: list[DocumentNode] = []
     variables: dict[str, list[str]] = {}
 
     def __init__(self, document_file: str = "") -> None:
@@ -98,6 +114,7 @@ class Document:
         self.parse_contents()
 
     def parse_contents(self) -> None:
+        """Parses all contents"""
         lines_builder = []
         for line in self.contents:
             line = line.strip()
@@ -141,35 +158,72 @@ class Document:
         return (variable, var_val_list)
 
     def build_tree(self) -> None:
-        # TODO:
-        last_level = 0
-
+        node_stack = []
         for line in self.contents:
             line_node = DocumentNode(line)
             if line_node.good_line is False:
                 continue
-
-            # if int(line[1]) == last_level +1:
-
-        _ = [print(x) for x in self.contents]
+            if len(self.root_nodes) == 0:
+                if line_node.level != 0:
+                    print(f"Error: First line level must be 0: {line}")
+                    break
+            if line_node.level == 0:
+                self.root_nodes += [line_node]
+                node_stack = []
+            elif line_node.level > node_stack[-1].level + 1:
+                print(
+                    f"Error: skip in line level from {node_stack[-1]} to {line_node.level}: {line_node.doc_line}"
+                )
+            elif line_node.level == node_stack[-1].level + 1:
+                node_stack[-1].child_nodes += [line_node]
+            elif line_node.level == node_stack[-1].level:
+                node_stack.pop()
+                node_stack[-1].child_nodes += [line_node]
+            else:
+                while line_node.level != node_stack[-1]:
+                    node_stack.pop()
+                node_stack[-1].child_nodes += [line_node]
+            node_stack += [line_node]
 
 
 class DialogBot:
     listener: Listener
     speaker: Speaker
     document: Document
+    active_nodes: list[DocumentNode]
+    active_variables = {"_": False}
+    variables: dict[str, list[str]] = {}
 
     def __init__(
         self, listener: Listener, speaker: Speaker, document: Document
     ) -> None:
-        pass
         self.listener = listener
         self.speaker = speaker
         self.document = document
 
     def run(self) -> None:
         # TODO
-        pass
+
+        # document root nodes are active
+        # Get input, compare it to inputs in each active node
+        # if it matches, we "speak" output
+        # Deactivate parent nodes, activate subnodes
+        # Handle variables
+
+        _input = self.listener.get_input()
+        for node in self.active_nodes:
+            for input_option in node.inputs:
+                if "_" in input_option:
+                    self.active_variables["_"] = True
+
+                    # input, compare to active node
+
+                # if "~" in _input:
+                #     self.active_variables["~"] = True
+                # if "#" in _input:
+                #     self.active_variables["#"] = True
+                # if "*" in _input:
+                #     self.active_variables["*"] = True
 
 
 if __name__ == "__main__":
@@ -177,4 +231,4 @@ if __name__ == "__main__":
     speaker = Speaker(audio=False)
     document = Document("dialog/test_file1.txt")
     dialog_bot = DialogBot(listener, speaker, document)
-    dialog_bot.run()
+    # dialog_bot.run()
