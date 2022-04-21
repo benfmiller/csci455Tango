f68bb8ec380be53a1de0f77878494345292f1f16
Ben Miller <benfmiller132@gmail.com>
2022-04-20 22:48:17 -0600

    setting up activate and clear

--- gui/main_gui.py
+++ gui/main_gui.py
@@ -13,23 +13,75 @@ from kivy.uix.button import Button
 from kivy.properties import BooleanProperty, ListProperty
 from kivy.animation import Animation
 
+# 1. Motors with speed, time and direction.
+# 2. Motors turn robot left, or right for x amount of seconds.
+# 3. Head tilt both directions
+# 4. Head pan both directions
+# 5. Waist turn both directions
+# 6. A wait for human speech input
+# 7. Talking, be able to type in what sentence you want to say and the robot says it.
 
-class PongBall(Widget):
-    velocity_x = NumericProperty(0)
-    velocity_y = NumericProperty(0)
-    velocity = ReferenceListProperty(velocity_x, velocity_y)
+categories = {
+    "drive": [],
+    "turn": [],
+    "head_tilt": [],
+    "head_turn": [],
+    "waist_turn": [],
+    "input": [],
+    "speak": [],
+}
 
-    def move(self):
-        self.pos = Vector(*self.velocity) + self.pos
+
+class ClearButton(Button):
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            print("Clearing placeholders")
+            app = App.get_running_app()
+            num = 8
+            for button in app.root.ids.placeHolderLayout.children:
+                button.reset()
+                button.text = str(num)
+                num -= 1
+        return super().on_touch_down(touch)
+
+
+class ActivateButton(Button):
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            print("Activating setup!")
+            app = App.get_running_app()
+            for button in app.root.ids.placeHolderLayout.children[::-1]:
+                print("Running ")
+                print(button.text)
+        return super().on_touch_down(touch)
 
 
 class PlaceHolderButton(Button):
+    num = 0
+    category = None
+
     def on_touch_down(self, touch):
-        print("I've been touched!")
+        if self.collide_point(*touch.pos):
+            print("I've been touched!")
         return super().on_touch_down(touch)
 
     def extra_func(self):
         print("Calling extra_func")
+        self.text = "blah"
+        self.background_normal = "./hello.jpeg"
+
+    def reset(self):
+        # TODO: reset text
+        self.category = None
+        # print(self.parent.ids)
+
+        # print(self.find_id)
+
+    # def find_id(self, parent, widget):
+    #     for id, obj in parent.ids.items():
+    #         if obj == widget:
+    #             print(id)
+    #             return id
 
 
 class ActionWidge(DragBehavior, Button):
@@ -58,8 +110,8 @@ class ActionWidge(DragBehavior, Button):
         if self.dragging:
             self.opacity = 1
             self.dragging = False
-            if self.collide_widget(app.root.ids.button9):
-                app.root.ids["button9"].extra_func()
+            if self.collide_widget(app.root.ids["place1"]):
+                app.root.ids["place1"].extra_func()
             anim = Animation(pos=self.original_pos, duration=0.0)
             anim.start(self)
         return super().on_touch_up(touch)
@@ -67,17 +119,8 @@ class ActionWidge(DragBehavior, Button):
 
 class TangoApp(App):
     def build(self):
-        # return MainLayout()
         return super().build()
 
 
 if __name__ == "__main__":
     TangoApp().run()
-
-# 1. Motors with speed, time and direction.
-# 2. Motors turn robot left, or right for x amount of seconds.
-# 3. Head tilt both directions
-# 4. Head pan both directions
-# 5. Waist turn both directions
-# 6. A wait for human speech input
-# 7. Talking, be able to type in what sentence you want to say and the robot says it.
