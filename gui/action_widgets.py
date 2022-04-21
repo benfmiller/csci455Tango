badafb33248e872038d89c69c7cbafb50d349b6c
Ben Miller <benfmiller132@gmail.com>
2022-04-21 13:57:59 -0600

    separating into two files

---
+++ gui/action_widgets.py
@@ -0,0 +1,290 @@
+import time
+import pyttsx3
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+# from kivy.properties import NumericProperty, ReferenceListProperty
+# from kivy.vector import Vector
+from kivy.uix.behaviors import DragBehavior
+
+# from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
+
+# from kivy.uix.gridlayout import GridLayout
+
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+
+
+# Simple drag from a boxlayout onto a drop zone, animate the return if the drop zone is missed.
+from kivy.properties import BooleanProperty, ListProperty
+from kivy.animation import Animation
+from kivy.uix.vkeyboard import VKeyboard
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.bubble import Bubble, BubbleButton
+
+from kivy.uix.textinput import TextInput
+
+actually_speak = False
+actually_listen = False
+actually_move = False
+
+
+class Speaker:  # output
+    engine: pyttsx3.Engine
+    using_console = False
+
+    def __init__(self) -> None:
+        if actually_speak is True:
+            self.engine = pyttsx3.init()
+            voices = self.engine.getProperty("voices")
+            # i corresponds with the voice type
+            i = 10
+            self.engine.setProperty("voice", voices[i].id)
+            print(f'Using voice: {self.engine.getProperty("voice")}')
+            self.engine.setProperty("rate", 150)
+        else:
+            self.using_console = True
+
+    def output(self, output: str):
+        if self.using_console is False:
+            self.engine.say(output)
+            self.engine.runAndWait()
+        print(f"Robot: {output}")
+
+
+class CustomBubbleButton(BubbleButton):
+    def add_text(self):
+        app = App.get_running_app()
+        index = app.root.text_input.cursor[0] - 1
+        if self.text != "⌫":
+            app.root.text_input.text = (
+                app.root.text_input.text[: index + 1]
+                + self.text
+                + app.root.text_input.text[index + 1 :]
+            )
+            app.root.text_input.cursor = (index + 2, 0)
+        else:
+            app.root.text_input.text = (
+                app.root.text_input.text[:index] + app.root.text_input.text[index + 1 :]
+            )
+            app.root.text_input.cursor = (index, 0)
+
+    pass
+
+
+class NumericKeyboard(Bubble):
+    def on_touch_up(self, touch):
+        app = App.get_running_app()
+        if not self.collide_point(*touch.pos) and not app.root.text_input.collide_point(
+            *touch.pos
+        ):
+            app.root.remove_widget(app.root.bubb)
+            app.root.text_input.focus = False
+            delattr(app.root, "bubb")
+
+    def __init__(self, **kwargs):
+        super(NumericKeyboard, self).__init__(**kwargs)
+        self.create_bubble_button()
+
+    def create_bubble_button(self):
+        numeric_keypad = ["7", "8", "9", "4", "5", "6", "1", "2", "3", ".", "0", "⌫"]
+        for x in numeric_keypad:
+            bubb_btn = CustomBubbleButton(text=str(x))
+            self.layout.add_widget(bubb_btn)
+
+
+class BubbleShowcase(FloatLayout):
+    def show_bubble(self, *l):
+        if not hasattr(self, "bubb"):
+            self.bubb = NumericKeyboard()
+            self.bubb.arrow_pos = "bottom_mid"
+            self.add_widget(self.bubb)
+
+
+class ActionWidge(DragBehavior, Button):
+    dragging = BooleanProperty(False)
+    original_pos = ListProperty()
+    after_delay = 0.2
+
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            print("on touch down")
+            self.original_pos = self.pos
+        return super().on_touch_down(touch)
+
+    def on_touch_move(self, touch):
+        if touch.grab_current is self:
+            self.opacity = 0.4
+            self.dragging = True
+        return super().on_touch_move(touch)
+
+    def collide_widget(self, wid):
+        if abs(self.center_x - wid.center_x) < wid.width / 2:
+            if self.top < wid.y:
+                return False
+            if self.y > wid.top:
+                return False
+            return True
+        return False
+
+    def on_touch_up(self, touch):
+        app = App.get_running_app()
+        # print(app.root.ids)
+        if self.dragging:
+            self.opacity = 1
+            self.dragging = False
+            for place_widge in app.root.ids.placeHolderLayout.children:
+                if self.collide_widget(place_widge):
+                    place_widge.set_action(self)
+            anim = Animation(pos=self.original_pos, duration=0.0)
+            anim.start(self)
+        return super().on_touch_up(touch)
+
+    def activate(self) -> None:
+        print("Base Button Activated")
+
+    def set_settings(self, settings_layout: BoxLayout):
+        newButton = Button()
+        newButton.text = "Nothing to do here!\nI'm a base button!"
+        settings_layout.add_widget(newButton)
+
+    def __str__(self) -> str:
+        return "Base Action Widge" + super().__str__()
+
+    def get_class_name(self) -> str:
+        return str(self.__class__).split(".")[-1]
+
+
+class DriveWidge(ActionWidge):
+    drive_speed: int
+    duration: float
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.drive_speed = 0
+        self.duration = 0
+
+    def __str__(self) -> str:
+        return f"{self.get_class_name()}: speed {self.drive_speed}: seconds {self.duration}: after_delay {self.after_delay}"
+
+    def set_settings(self, settings_layout: BoxLayout):
+        # VKeyboard.layout = "numeric"
+        # player = VKeyboard()
+        # player.type
+        newButton = Button()
+        newButton.text = "Nothing to do here!\nI'm a base button!"
+        settings_layout.add_widget(TextInput())
+
+
+class TurnWidge(ActionWidge):
+    turn_speed: int
+    duration: float
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.turn_speed = 0
+        self.duration = 0
+
+    def __str__(self) -> str:
+        return f"{self.get_class_name()}: speed {self.turn_speed}: seconds {self.duration}: after_delay {self.after_delay}"
+
+
+class HeadTiltWidge(ActionWidge):
+    position: int
+    duration: float
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.position = 0
+        self.duration = 0
+
+    def __str__(self) -> str:
+        str(self.__class__).split(".")[-1]
+        return f"{self.get_class_name()}: position {self.position}: seconds {self.duration}: after_delay {self.after_delay}"
+
+
+class HeadTurnWidge(ActionWidge):
+    position: int
+    duration: float
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.position = 0
+        self.duration = 0
+
+    def __str__(self) -> str:
+        str(self.__class__).split(".")[-1]
+        return f"{self.get_class_name()}: position {self.position}: seconds {self.duration}: after_delay {self.after_delay}"
+
+
+class WaistWidge(ActionWidge):
+    position: int
+    duration: float
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.position = 0
+        self.duration = 0
+
+    def __str__(self) -> str:
+        str(self.__class__).split(".")[-1]
+        return f"{self.get_class_name()}: speed {self.position}: seconds {self.duration}: after_delay {self.after_delay}"
+
+
+class InputWidge(ActionWidge):
+    input_string: str
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.input_string = "Nothing"
+
+    def __str__(self) -> str:
+        str(self.__class__).split(".")[-1]
+        return f"{self.get_class_name()}: Wait for string '{self.input_string}': after_delay {self.after_delay}"
+
+
+class OutputWidge(ActionWidge):
+    my_speaker = Speaker()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.output_widge = TextInput()
+        self.output_widge.text = "I have nothing to say"
+
+    def __str__(self) -> str:
+        str(self.__class__).split(".")[-1]
+        return f"{self.get_class_name()}: Will say '{self.output_widge.text}': after_delay {self.after_delay}"
+
+    def set_settings(self, settings_layout: BoxLayout):
+        # VKeyboard.layout = "numeric"
+        # player = VKeyboard()
+        # player.type
+        inner_layout = BoxLayout()
+
+        block = BoxLayout()
+        block.size_hint_x = 0.6
+        block.orientation = "vertical"
+        label = Label()
+        label.text = "Input text to speak"
+        label.size_hint_y = 0.2
+        block.add_widget(label)
+        block.add_widget(self.output_widge)
+
+        block2 = BoxLayout()
+        block2.size_hint_x = 0.4
+        block2.orientation = "vertical"
+        label = Label()
+        label.text = "After Delay Seconds"
+        label.size_hint_y = 0.2
+        block2.add_widget(label)
+        block2.add_widget(NumericKeyboard())
+
+        inner_layout.add_widget(block)
+        inner_layout.add_widget(block2)
+
+        settings_layout.add_widget(inner_layout)
+
+    def activate(self) -> None:
+        print("Speaking")
+        self.my_speaker.output(self.output_widge.text)
