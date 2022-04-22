408f2019e007ca5d46039f018779921f2d5b8307
Ben Miller <benfmiller132@gmail.com>
2022-04-22 01:19:17 -0600

    implemented return from animation screen

--- gui/main_gui.py
+++ gui/main_gui.py
@@ -52,23 +52,66 @@ class ClearButton(Button):
         return super().on_touch_down(touch)
 
 
-def activate_thread():
-    # time.sleep(0.05)
-
+def set_activate_screen():
     app = App.get_running_app()
-    # num = len(app.root.ids.placeHolderLayout.children)
+    activate_layout = app.root.ids.activateWidgetLayout
+    for widge in list(activate_layout.children):
+        activate_layout.remove_widget(widge)
+    new_widge_list = list(app.root.ids.placeHolderLayout.children)[::-1]
+    placeholder_layout = app.root.ids.placeHolderLayout
+    for widge in list(app.root.ids.placeHolderLayout.children):
+        placeholder_layout.remove_widget(widge)
+    for widge in new_widge_list:
+        activate_layout.add_widget(widge)
+
+
+def set_main_placeholders_back():
+    app = App.get_running_app()
+    placeholder_layout = app.root.ids.placeHolderLayout
+    for widge in list(placeholder_layout.children):
+        placeholder_layout.remove_widget(widge)
+    new_widge_list = list(app.root.ids.activateWidgetLayout.children)[::-1]
+    activate_layout = app.root.ids.activateWidgetLayout
+    print(activate_layout.children)
+    for widge in list(activate_layout.children):
+        activate_layout.remove_widget(widge)
+    for widge in new_widge_list:
+        placeholder_layout.add_widget(widge)
+
+
+def activate_thread():
+    app = App.get_running_app()
     num = 1
-    for button in app.root.ids.placeHolderLayout.children[::-1]:
+    app.root.ids.backToMainScreen.disabled = True
+    for button in app.root.ids.activateWidgetLayout.children[::-1]:
         print(f"Running box {num}: category {button.action}")
         # temp_background = button.background_normal
         # button.background_normal = ""
-        button.background_color = (1.0, 1.0, 0.0, 1.0)
+        button.background_color = (1.0, 1.0, 0.2, 1.0)
         if button.action is not None:
             button.action.activate()
         time.sleep(0.5)
         button.background_color = (1.0, 1.0, 1.0, 1.0)
         # button.background_normal = temp_background
         num += 1
+
+    app.root.ids.backToMainScreen.disabled = False
+    if root_robot is not None:
+        print("Neutralizing robot")
+        root_robot.neutral()
+        time.sleep(0.05)
+        root_robot.neutral()
+        time.sleep(0.05)
+        root_robot.neutral()
+        time.sleep(0.05)
+        root_robot.forward()
+        time.sleep(0.05)
+        root_robot.stop()
+        time.sleep(0.05)
+        root_robot.backward()
+        time.sleep(0.05)
+        root_robot.stop()
+        print("********Done Neutralizing********\n")
 
 
 class ActivateButton(Button):
@@ -80,30 +123,23 @@ class ActivateButton(Button):
         # self.bind(on_press=self.callback)
 
     def on_touch_down(self, touch):
-        global running_activator
         if self.collide_point(*touch.pos) and time.time() - self.last_ran > 1:
             print("\n********Activating setup********!")
+            app = App.get_running_app()
+            set_activate_screen()
+            app.root.current = "activateScreen"
             Thread(target=activate_thread).start()
             print("********Activation Done********\n")
-
-            if root_robot is not None:
-                print("Neutralizing robot")
-                root_robot.neutral()
-                time.sleep(0.05)
-                root_robot.neutral()
-                time.sleep(0.05)
-                root_robot.neutral()
-                time.sleep(0.05)
-                root_robot.forward()
-                time.sleep(0.05)
-                root_robot.stop()
-                time.sleep(0.05)
-                root_robot.backward()
-                time.sleep(0.05)
-                root_robot.stop()
-                print("********Done Neutralizing********\n")
             self.last_ran = time.time()
+        return super().on_touch_down(touch)
 
+
+class ReturnScreenButton(Button):
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos) and not self.disabled:
+            app = App.get_running_app()
+            set_main_placeholders_back()
+            app.root.current = "mainScreen"
         return super().on_touch_down(touch)
 
 
@@ -119,7 +155,6 @@ class PlaceHolderButton(Button):
         return super().on_touch_down(touch)
 
     def set_settings_screen(self):
-        # TODO:
         app = App.get_running_app()
         settings_layout = app.root.ids.settingsLayout
         for widge in list(settings_layout.children):
@@ -137,11 +172,7 @@ class PlaceHolderButton(Button):
 
     def set_action(self, actionWidge: ActionWidge):
         self.action = actionWidge
-
-        # TODO: add action images
         self.text = actionWidge.text
-        # print(self.background_normal)
-        # print(type(self.background_normal))
         self.background_normal = actionWidge.background_normal
 
     def reset(self):
